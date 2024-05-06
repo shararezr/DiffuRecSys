e167e049707ed1390f7402cbb28339b1216d0552
Sharare Zolghadr <zr.sharare@gmail.com>
2024-05-06 14:35:25 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -7,7 +7,7 @@
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
-#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -gpu "num=1"
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
